87f1fd8fc7c0e328d7d435b56f3fdbd325dac745
unknown <marian.b.pastran@gmail.com>
2022-02-01 13:21:55 -0300

    elimine <li> y agregue la nueva lista de cosas aprendidas

--- index.js
+++ index.js
@@ -25,6 +25,28 @@ const cosasQueAprendimos = [
   },
 ];
 
-function main() {}
+function main() {
+  const listA= document.querySelector(".lista");
+  const list= document.querySelectorAll(".lista li");
+  
+  for (const el of list) {
+    el.remove();
+
+  }
+
+
+  for (const elem of cosasQueAprendimos) {
+    //creo primero el elementi <li>
+    const newLi= document.createElement("li");
+    //le agrego el texto (es necesario ponr la igualdad)
+    newLi.textContent = elem.tema;
+    //agrego la clase
+
+    newLi.classList.add(elem.class || "item");
+    
+    // lo agrego a la lista
+    listA.append(newLi);
+  }
+}
 
 main();
